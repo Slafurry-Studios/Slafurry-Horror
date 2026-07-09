7f508f04aed1d41b1de5f40ffc1a6a5f4e50b17f
junN0ir <junniormarcellino20@gmail.com>
2026-07-09 19:18:50 +0800

    improve interaction system

--- Assets/_Game/00_Scripts/Manager/InspectManager.cs
+++ Assets/_Game/00_Scripts/Manager/InspectManager.cs
@@ -4,7 +4,7 @@ public class InspectManager : MonoBehaviour
     public static InspectManager instance;
     [Header("Pengaturan Default Semua Objek Inspectable")]
     [Tooltip("Gunakan {0} untuk menyisipkan nama objek secara otomatis.")]
-    public string promptFormat = "Tekan E untuk periksa {0}";
+    public string promptFormat = "Press E to inspect {0}";
     public float examineDistance = 0.5f;
     public float examineScaleMultiplier = 1f;
     void Awake()
